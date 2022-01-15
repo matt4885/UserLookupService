0e24e1f490cbcfb5150270df6a6819645e1ce36f
Matthew Kempey <mkempey@gmail.com>
2022-01-15 15:33:05 -0500

    Added SQL to Data project

---
+++ backend/UserLookupService.Data/Migrations/Sql/AddUserTable.sql
@@ -0,0 +1,16 @@
+﻿START TRANSACTION;
+
+CREATE TABLE "Users" (
+    "Id" uuid NOT NULL,
+    "GivenName" text NULL,
+    "FamilyName" text NULL,
+    "Email" text NULL,
+    "DateOfBirth" timestamp with time zone NOT NULL,
+    "Address" text NULL,
+    CONSTRAINT "PK_Users" PRIMARY KEY ("Id")
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20220115202149_AddUserTable', '6.0.1');
+
+COMMIT;
