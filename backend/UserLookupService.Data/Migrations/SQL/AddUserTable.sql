2afdf8aee3a90a5e4668ec312d1f42f657a84033
Matthew Kempey <mkempey@gmail.com>
2022-01-21 00:23:49 -0500

    Fixed SQL

---
+++ backend/UserLookupService.Data/Migrations/SQL/AddUserTable.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    migration_id character varying(150) NOT NULL,
+    product_version character varying(32) NOT NULL,
+    CONSTRAINT pk___ef_migrations_history PRIMARY KEY (migration_id)
+);
+
+START TRANSACTION;
+
+CREATE TABLE users (
+    id uuid NOT NULL,
+    given_name text NULL,
+    family_name text NULL,
+    email text NULL,
+    date_of_birth timestamp with time zone NOT NULL,
+    address text NULL,
+    CONSTRAINT pk_users PRIMARY KEY (id)
+);
+
+INSERT INTO "__EFMigrationsHistory" (migration_id, product_version)
+VALUES ('20220121011205_AddUserTable', '6.0.1');
+
+COMMIT;
