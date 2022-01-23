21716a64ecf14bd96c4c273627043ce2eaa5b0ee
Matthew Kempey <mkempey@gmail.com>
2022-01-23 14:01:31 -0500

    Cleanup MainContext and move EF Configuration to Fluent API

--- backend/UserLookupService.Data/MainContext.cs
+++ backend/UserLookupService.Data/MainContext.cs
@@ -12,4 +12,27 @@ public class MainContext : DbContext
     {
 
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        UserTable(modelBuilder);
+    }
+
+    private static void UserTable(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().ToTable("User");
+        modelBuilder.Entity<User>().HasKey(u => u.Id);
+        modelBuilder.Entity<User>().Property(u => u.Address)
+            .IsRequired()
+            .HasMaxLength(95);
+        modelBuilder.Entity<User>().Property(u => u.GivenName)
+            .IsRequired()
+            .HasMaxLength(50);
+        modelBuilder.Entity<User>().Property(u => u.FamilyName)
+            .IsRequired()
+            .HasMaxLength(50);
+        modelBuilder.Entity<User>().Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(128);
+    }
 }
